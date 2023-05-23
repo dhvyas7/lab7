56e8409ddef8f529e32a72a5c2a3bd285a2f8c32
Dhyan Vyas <cs15lsp23hb@ieng6-203.ucsd.edu>
2023-05-22 17:54:26 -0700

    Fixed Code For Testing

--- ListExamples.java
+++ ListExamples.java
@@ -24,6 +24,8 @@ class ListExamples {
   static List<String> merge(List<String> list1, List<String> list2) {
     List<String> result = new ArrayList<>();
     int index1 = 0, index2 = 0;
+
+    int x=1;
     while(index1 < list1.size() && index2 < list2.size()) {
       if(list1.get(index1).compareTo(list2.get(index2)) < 0) {
         result.add(list1.get(index1));
@@ -45,6 +47,6 @@ class ListExamples {
     }
     return result;
   }
-
+//commit
 
 }
